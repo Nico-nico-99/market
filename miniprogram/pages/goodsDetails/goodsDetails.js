c924dd2ddb8b8a35ec1ca4cab7e55168a2eec24b
玖玖 <3313593165@qq.com>
2020-10-31 10:52:48 +0800

    10.31
    添加商品详情页按钮事件

--- miniprogram/pages/goodsDetails/goodsDetails.js
+++ miniprogram/pages/goodsDetails/goodsDetails.js
@@ -5,17 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-  "good":{
-    "cm_id": 25,
-    "picture_url1": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603098830639&di=b44a9d7c9a96fa8446df941aeeea0fba&imgtype=0&src=http%3A%2F%2Fimg.book118.com%2Fsr1%2FM00%2F29%2F3D%2FwKh2AlvnQSiIR2BKABBsKNt0issAAQZFwBMTpgAEGxA391.png",
-    "name": "线性代数",
-    "price": 25.00,
-    "address": 1,
-    "is_new": 1,
-    "classify": 1,
-    "contact_information":"12345678912",
-    "details":"线性代数是数学的一个分支，它的研究对象是向量，向量空间（或称线性空间），线性变换和有限维的线性方程组。向量空间是现代数学的一个重要课题；因而，线性代数被广泛地应用于抽象代数和泛函分析中；通过解析几何，线性代数得以被具体表示。线性代数的理论已被泛化为算子理论。由于科学研究中的非线性模型通常可以被近似为线性模型，使得线性代数被广泛地应用于自然科学和社会科学中。"
+    "good":{
+      "cm_id": 25,
+      "picture_url1": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603098830639&di=b44a9d7c9a96fa8446df941aeeea0fba&imgtype=0&src=http%3A%2F%2Fimg.book118.com%2Fsr1%2FM00%2F29%2F3D%2FwKh2AlvnQSiIR2BKABBsKNt0issAAQZFwBMTpgAEGxA391.png",
+      "name": "线性代数",
+      "price": 25.00,
+      "address": 1,
+      "is_new": 1,
+      "classify": 1,
+      "contact_information":"12345678912",
+      "details":"线性代数是数学的一个分支，它的研究对象是向量，向量空间（或称线性空间），线性变换和有限维的线性方程组。向量空间是现代数学的一个重要课题；因而，线性代数被广泛地应用于抽象代数和泛函分析中；通过解析几何，线性代数得以被具体表示。线性代数的理论已被泛化为算子理论。由于科学研究中的非线性模型通常可以被近似为线性模型，使得线性代数被广泛地应用于自然科学和社会科学中。"
+    },
   },
+
+  /* 收藏按钮事件 */
+  collect: function(e){
+    console.log("收藏该商品，商品id为：" + this.data.good.cm_id)
+  },
+
+  /* 预订按钮事件 */
+  reserve: function(e){
+    console.log("预订该商品，商品id为：" + this.data.good.cm_id)
   },
 
   /**
