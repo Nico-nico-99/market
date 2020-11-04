8a95077e37013583157d59d90514fed7638f501b
AuroraHe <72778691+AuroraHe@users.noreply.github.com>
2020-11-04 18:09:13 +0800

    Add files via upload

--- miniprogram/pages/userCenter/userBookings/userBookings.js
+++ miniprogram/pages/userCenter/userBookings/userBookings.js
@@ -1,11 +1,74 @@
 // miniprogram/pages/userCenter/userBookings/userBookings.js
+
+import goodsListItem2 from '../../../templates/goodsListItem2/goodsListItem2'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    //搜索栏
+    searchInput: '',
 
+    //预订商品列表
+    goodsList: [
+      {
+        "cm_id": 25,
+        "name": "线性代数",
+        "picture_url1": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603098830639&di=b44a9d7c9a96fa8446df941aeeea0fba&imgtype=0&src=http%3A%2F%2Fimg.book118.com%2Fsr1%2FM00%2F29%2F3D%2FwKh2AlvnQSiIR2BKABBsKNt0issAAQZFwBMTpgAEGxA391.png",
+        "price": 25.50,
+        "address": 1,
+        "is_new": 0,
+        "classify": 0
+      },
+      {
+        "cm_id": 26,
+        "name": "喵喵喵",
+        "picture_url1": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603099709041&di=cbfa63df06fb032abf3fb9074f180000&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201807%2F02%2F20180702073005_nqjxx.jpg",
+        "price": 25.00,
+        "address": 2,
+        "is_new": 0,
+        "classify": 1
+      },
+      {
+        "cm_id": 27,
+        "name": "作业爆炸多",
+        "picture_url1": "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1313956600,1942141254&fm=26&gp=0.jpg",
+        "price": 25.00,
+        "address": 3,
+        "is_new": 1,
+        "classify": 2
+      }
+    ],
+  },
+
+  /**
+   * 获取输入框内容
+   */
+  bindSearchInput: function (e) {
+    this.searchInput = e.detail.value
+  },
+
+  /**
+   * 预订商品搜索
+   */
+  gotoSearch: function(e){
+    console.log("预订商品搜索: ", this.searchInput)
+  },
+
+  /**
+   * 用户付款
+   */
+  payment: function(e){
+    goodsListItem2.payment(e)
+  },
+
+  /**
+   * 用户取消订单
+   */
+  cancelOrder: function(e){
+    goodsListItem2.cancelOrder(e)
   },
 
   /**
